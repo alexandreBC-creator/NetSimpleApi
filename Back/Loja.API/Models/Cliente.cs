2bdc4312eacb33efa09770a853a54b2da4fd7d50
RodrigoLucas1707 <rodrigo07ssp@live.com>
2021-08-24 21:52:23 -0300

    Cliente

--- Back/Loja.API/Models/Cliente.cs
+++ Back/Loja.API/Models/Cliente.cs
@@ -1,35 +1,47 @@
 using System;
 
 namespace Loja.API.Models {
-    public class Produto {
+    public class Cliente {
         public int? Id { get; set; }
         public string Nome { get; set; }
-        public int Estoque { get; set; }
-        private double _valor;
-        public double Valor { 
-            get{ return this._valor; }
-            set { this._valor = (value < 0 ? 0 : value);} 
+        public bool Liberado { get; set; }
+        private double _credito;
+        public double Credito { 
+            get{ return this._credito; }
+            set { this._credito = (value < 0 ? 0 : value);} 
         }
         public DateTime DataCadastro { get; set; }
-        public DateTime DataAtualizacao { get; set; }
+        public DateTime DataNascimento { get; set; }
 
-        public Produto() {
+        public Cliente() {
             DataCadastro = DateTime.Now;
-            DataAtualizacao = DateTime.Now;
         }
-        public Produto(string nome, int estoque, double valor){
+        public Cliente(string nome, double credito){
             this.Id = null;
             this.Nome = nome;
-            this.Estoque = estoque;
-            this.Valor = valor;
+            this.Liberado = true;
+            this.Credito = credito;
             this.DataCadastro = DateTime.Now;
-            this.DataAtualizacao = DateTime.Now;
         }
-        public void AtualizarProduto(string nome, int estoque, double valor){
+        public Cliente(string nome, bool liberado, double credito, DateTime dataNascimento){
+            this.Id = null;
+            this.Nome = nome;
+            this.Liberado = liberado;
+            this.Credito = credito;
+            this.DataCadastro = DateTime.Now;
+            this.DataNascimento = dataNascimento;
+        }
+        public void Liberar(){
+            this.Liberado = true;
+        }
+        public void Bloquear(){
+            this.Liberado = false;
+        }
+        public void AtualizarCliente(string nome, bool liberado, double credito, DateTime dataNascimento){
             Nome = nome;
-            Estoque = estoque;
-            Valor = valor;
-            DataAtualizacao = DateTime.Now;
+            Liberado = liberado;
+            Credito = credito;
+            DataNascimento = dataNascimento;
         }
     }
 }
