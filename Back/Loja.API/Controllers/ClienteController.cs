7ed98aa2d20f4a532a8cea5382b9996330484962
alexandreBC-creator <alexandrebc666@outlook.com>
2021-08-24 22:03:32 -0300

    controller

--- Back/Loja.API/Controllers/ClienteController.cs
+++ Back/Loja.API/Controllers/ClienteController.cs
@@ -6,31 +6,31 @@ using Microsoft.AspNetCore.Mvc;
 namespace Loja.API.Controllers{
     [ApiController]
     [Route("api/[controller]")]
-    public class ProdutoController : ControllerBase{
-        public static List<Produto> produtos = new List<Produto>();
-        public ProdutoController(){
-            if(produtos.Count <= 0){
-                Produto produto = new Produto(){
+    public class CLienteController : ControllerBase{
+        public static List<CLiente> clientes = new List<CLiente>();
+        public CLienteController(){
+            if(clientes.Count <= 0){
+                CLiente cliente = new CLiente(){
                 Id = 1,
-                Nome = "Tênis",
-                Estoque = 10,
-                Valor = 159.99
+                Nome = "Marquinhos",
+                Liberado = true,
+                Credito = 400.00
                 };
-                produtos.Add(produto);
-                produto = new Produto(){
+                clientes.Add(cliente);
+                CLiente cliente = new CLiente(){
                 Id = 2,
-                Nome = "Camiseta",
-                Estoque = 5,
-                Valor = 59.99
+                Nome = "Mirosvaldo",
+                Liberado = true,
+                Credito = 810.00
                 };
-                produtos.Add(produto);
-                produto = new Produto(){
+                clientes.Add(cliente);
+                CLiente cliente = new CLiente(){
                 Id = 3,
-                Nome = "Boné",
-                Estoque = 7,
-                Valor = 39.99
+                Nome = "Maria Bonita",
+                Liberado = true,
+                Credito = 170.00
                 };
-                produtos.Add(produto);
+                clientes.Add(cliente);
             }
         }
         [HttpGet]
@@ -40,20 +40,20 @@ namespace Loja.API.Controllers{
             //     Estoque = 10,
             //     Valor = 159.99
             //};
-            return Ok(produtos);
+            return Ok(clientes);
         }
 
         [HttpGet("{id}")]
         public IActionResult Get(int id){
-            var produtoSelecionado = produtos.Where(
-                prod => prod.Id == id);
-            return Ok(produtoSelecionado);
+            var clienteSelecionado = clientes.Where(
+                cli => cli.Id == id);
+            return Ok(clienteSelecionado);
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody] Produto novoProduto){
-            produtos.Add(novoProduto);
-            return Created("", novoProduto);
+        public IActionResult Post([FromBody] CLiente novoCliente){
+            clientes.Add(novoCliente);
+            return Created("", novoCliente);
         }
 
         [HttpPut("{id}")]
@@ -64,10 +64,10 @@ namespace Loja.API.Controllers{
         [HttpDelete("{id}")]
         public IActionResult Delete(int id) {
             // Selecionar o produto que deverá ser removido
-            Produto produtoSelecionado = produtos.FirstOrDefault(p => p.Id == id);
-            if (produtoSelecionado != null){
+            CLiente clienteSelecionado = clientes.FirstOrDefault(p => p.Id == id);
+            if (clienteSelecionado != null){
                 // Remove o produto da lista
-                produtos.Remove(produtoSelecionado);
+                produtos.Remove(clienteSelecionado);
                 // Retorna um resultado para o cliente
                 return NoContent();
             }
