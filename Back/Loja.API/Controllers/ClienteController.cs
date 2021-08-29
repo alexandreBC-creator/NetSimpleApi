0b88c8e60406b379b25b77c9d457b01c885cbc24
RodrigoLucas1707 <rodrigo07ssp@live.com>
2021-08-29 13:11:14 -0300

    Final

--- Back/Loja.API/Controllers/ClienteController.cs
+++ Back/Loja.API/Controllers/ClienteController.cs
@@ -10,36 +10,31 @@ namespace Loja.API.Controllers{
         public static List<Cliente> clientes = new List<Cliente>();
         public ClienteController(){
             if(clientes.Count <= 0){
-                Cliente cliente1 = new Cliente(){
+                Cliente cliente = new Cliente(){
                 Id = 1,
                 Nome = "Marquinhos",
                 Liberado = true,
                 Credito = 400.00
                 };
-                clientes.Add(cliente1);
-                Cliente cliente2 = new Cliente(){
+                clientes.Add(cliente);
+                cliente = new Cliente(){
                 Id = 2,
                 Nome = "Mirosvaldo",
-                Liberado = true,
+                Liberado = false,
                 Credito = 810.00
                 };
-                clientes.Add(cliente2);
-                Cliente cliente3 = new Cliente(){
+                clientes.Add(cliente);
+                cliente = new Cliente(){
                 Id = 3,
                 Nome = "Maria Bonita",
                 Liberado = true,
                 Credito = 170.00
                 };
-                clientes.Add(cliente3);
+                clientes.Add(cliente);
             }
         }
         [HttpGet]
         public IActionResult Get(){
-            // Produto produto = new Produto(){
-            //     Nome = "Tênis",
-            //     Estoque = 10,
-            //     Valor = 159.99
-            //};
             return Ok(clientes);
         }
 
@@ -50,25 +45,25 @@ namespace Loja.API.Controllers{
             return Ok(clienteSelecionado);
         }
 
-         [HttpGet("{credito}")]
-        public IActionResult GetByCreditoMaiorOuIgual(double credito){
+          [HttpGet("/credito{credito}")]
+        public IActionResult GetMaiorOuIgual(double credito) {
             var clientesSelecionados = clientes.Where(
                 cli => cli.Credito >= credito);
             return Ok(clientesSelecionados);
         }
 
-        [HttpGet]
-        public IActionResult GetClientesBloqueados(){
-            var clientesSelecionados = clientes.Where(
-                cli => cli.Liberado == false);
-            return Ok(clientesSelecionados);
-        }
-
-        [HttpGet]
-        public IActionResult GetClientesLiberados(){
-            var clientesSelecionados = clientes.Where(
+        [HttpGet("/liberados")]
+        public IActionResult GetLiberados() {
+            var clienteSelecionado = clientes.Where(
                 cli => cli.Liberado == true);
-            return Ok(clientesSelecionados);
+            return Ok(clienteSelecionado);
+        }
+        
+        [HttpGet("/bloqueados")]
+        public IActionResult GetBloqueados() {
+            var clienteSelecionado = clientes.Where(
+                cli => cli.Liberado == false);
+            return Ok(clienteSelecionado);
         }
 
         [HttpPost]
@@ -84,12 +79,9 @@ namespace Loja.API.Controllers{
 
         [HttpDelete("{id}")]
         public IActionResult Delete(int id) {
-            // Selecionar o produto que deverá ser removido
             Cliente clienteSelecionado = clientes.FirstOrDefault(p => p.Id == id);
             if (clienteSelecionado != null){
-                // Remove o produto da lista
                 clientes.Remove(clienteSelecionado);
-                // Retorna um resultado para o cliente
                 return NoContent();
             }
             return NotFound();
